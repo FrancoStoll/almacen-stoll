a4d94aa8ae0099826364bd8a6bd9648a9222b8ca
FrancoStoll <francostoll@gmail.com>
2023-10-29 17:07:46 -0300

    ham-menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,14 +2,14 @@ import { useEffect } from "react";
 
 import { utcToZonedTime } from "date-fns-tz";
 
-
 import imagenLogo from "../assets/imagen-log.jpeg";
 import imagenHero from "../assets/imagen-google.jpg";
 import { useIsOpen } from "../hooks/useIsOpen";
+import { useState } from "react";
 
 const Navbar = () => {
   const { open, isMorningOpen, isAfternoon } = useIsOpen();
-
+  const [menu, setMenu] = useState(false);
   useEffect(() => {
     const now = new Date();
     const argentinaTimeZone = "America/Argentina/Buenos_Aires";
@@ -24,7 +24,23 @@ const Navbar = () => {
       <div className="navegacion">
         <img src={imagenLogo} alt="hola" />
 
-        <div className="anchor">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="menu-ham"
+          onClick={() => setMenu(!menu)}
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+          />
+        </svg>
+
+        <div className={`anchor ${menu ? 'show' : ''}`}>
           <a href="#">About</a>
           <a href="#">Hours</a>
           <a href="#">Conctact</a>
